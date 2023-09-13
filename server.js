fb4cf309fc4e67757fef6d2ed10664de09493311
saurabhsuresh <saurabhsuresh4s@gmail.com>
2023-09-13 13:58:57 +0400

    server spin down fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const movieRoutes = require('./routes/movie.js');
-
+const axios = require('axios'); // Import axios for sending requests
 var bodyParser = require('body-parser');
 require("dotenv").config();
 const cors = require("cors");
@@ -36,5 +36,16 @@ app.get(`${apiDir}`,function(request, response) {
     response.send('Hello, welcome to Movies + Server');
 });
 
-
-
+// Function to send a traffic request to your server
+async function sendTrafficRequest() {
+    try {
+      const response = await axios.get(`https://movies-app-server-ypl0.onrender.com`);
+      console.log('Traffic request sent successfully', response.status);
+    } catch (error) {
+      console.error('Error sending traffic request', error.message);
+    }
+  }
+  
+  // Set an interval to send the request every 14 minutes (840,000 milliseconds)
+  const interval = 14 * 60 * 1000;
+  setInterval(sendTrafficRequest, interval);

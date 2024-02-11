e9fea6d7839ca56f0b394b16559d391ddde96268
hackgods <saurabhsuresh4s@gmail.com>
2024-02-11 12:24:59 +0000

    redis reconnect bug fixed

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -2,7 +2,15 @@ const { createClient } = require('redis');
 require('dotenv').config();
 
 const client = createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
+  retry_strategy: function(options) {
+    if (options.attempt > 10) {
+      // Stop retrying after 10 attempts
+      return undefined;
+    }
+    // Wait 2 seconds before retrying
+    return 2000;
+  }
 });
 
 client.on('error', function (err) {
